170474db345b68bf84cd0a6e3cbf7a60f3b3dd1a
husnesompa <sompa.cse@gmail.com>
2024-09-24 17:46:20 +0600

    added functional part

--- js/button.js
+++ js/button.js
@@ -1,22 +1,56 @@
-document.getElementById('donationBtn').addEventListener('click', function(){
+// -------Button------
+document.getElementById('donationBtn').addEventListener('click', function () {
     showSectionById('donation_section')
 });
 
-document.getElementById('hitoryBtn').addEventListener('click', function(){
+document.getElementById('historyBtn').addEventListener('click', function () {
     showSectionById('history_section');
 });
 
+// ------Modal-----
+let modal = document.getElementById('my_modal_5');
+let modalCloseButton = document.getElementById('modal_close_btn');
 
-document.getElementById('donate_now_button').addEventListener('click', function(event){
-   event.preventDefault();
-
-   const inputDonateMoney = getInputFieldById('donation_input');
-   const totalAmount = getTagValueById('total_amount');
-   if (totalAmount > inputDonateMoney && inputDonateMoney > 0 && typeof inputDonateMoney === 'number' && !isNaN(inputDonateMoney)) {
-    console.log("ok");
-} else {
-    console.log("Please input correct amount") ;
-}
-
+modalCloseButton.addEventListener('click', function () {
+    modal.close();   // Close the modal
 });
+
+// -------Donation button , validation, calculation , transaction history and modal ------
+document.getElementById('donate_now_button').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const inputDonateMoney = getInputFieldById('donation_input');
+    const totalAmount = getTextById('total_amount');
+    const donationAmount = getTextById('donation_amount');
+
+    if (totalAmount > inputDonateMoney && inputDonateMoney > 0 && !isNaN(inputDonateMoney)) {
+
+        // ------Calculation part -----
+        const totalDonationAmount = donationAmount + inputDonateMoney;
+        const availableAmount = totalAmount - inputDonateMoney
+
+        // -------Display amount in UI -----
+        document.getElementById('donation_amount').innerText = totalDonationAmount;
+        document.getElementById('total_amount').innerText = availableAmount;
+
+        // -----Donation History -----
+        const donateTime = forTime();
+        const tittle = forTittle(this);
+        const div = document.createElement('div');
+        div.classList.add('container', 'mx-auto', 'w-full', 'border-2', "p-4", "my-4", "rounded-lg");
+        div.innerHTML = `
+   <h4 class="text-xl font-bold">${inputDonateMoney} Taka is  ${tittle} </h4>
+   <p id="displayTime">Date: ${donateTime} </p>
+   `
+        document.getElementById('history_section').appendChild(div);
+
+        // ----modal---
+        modal.showModal();
+    }
+    else {
+        alert("Please input correct amount");
+    }
+});
+
+
 
--- js/common.js
+++ js/common.js
@@ -1,3 +1,4 @@
+// -------Button-----
 function showSectionById(id){
     //  ----Hidden all id-----
     document.getElementById('donation_section').classList.add('hidden');
@@ -6,12 +7,32 @@ function showSectionById(id){
     document.getElementById(id).classList.remove('hidden');
 }
 
+// -------Data from Input Feild------
 function getInputFieldById(id){
     const InputValue = document.getElementById(id).value;
-    return InputValue;
+    const inputValueToNumber = parseFloat(InputValue);
+    return inputValueToNumber;
 }
-function getTagValueById(id){
+
+// -------Data from HTML Tag------
+function getTextById(id){
     const tagValue = document.getElementById(id).innerText;
     const intTagValue = parseFloat(tagValue);
     return intTagValue;
+}
+
+// -------Time and Date------
+function forTime(){
+    const currentTime = new Date();
+    const formattedTime = currentTime.toTimeString();
+    const formattedDate = currentTime.toUTCString();
+    return formattedDate + ' ' + formattedTime ;
+}
+
+// -------Data from Parent Element------
+function forTittle(id){
+    const parentDiv = id.parentElement;
+    const h2Element = parentDiv.querySelector('h2');
+    const tittleText = h2Element.textContent;
+    return tittleText;
 }
